60e50fb479196524b6eb6b127fe30d99913fe393
Francisco Baio Dias <xicombd@gmail.com>
2014-01-17 22:44:04 +0000

    pixi cool!

--- js/Frame.js
+++ js/Frame.js
@@ -2,37 +2,32 @@
 ** GAME FRAME CLASS
 **************************************************/
 var Frame = function(id) {
-	var canvas,			// Canvas DOM element
-		ctx,			// Canvas rendering context
-	//	keys,			// Keyboard input
+	var stage,			// PIXI Stage element
+		renderer,		// PIXI Renderer
 		localPlayer;	// Local player
 
 	var init = function() {
-		canvas = document.createElement('canvas');
-		ctx = canvas.getContext("2d");
-		canvas.id     = "CursorLayer";
-		canvas.width  = window.innerWidth/frames.length+2;
-		canvas.height = window.innerHeight;
-		canvas.style.position = "absolute";
-		canvas.style.top="0px";
-		canvas.style.left=frames*"px";
-		canvas.style.zIndex   = 8;
-		canvas.style.border   = "1px solid";
-		document.body.appendChild(canvas);
+		stage = new PIXI.Stage(0xEEFFFF);
+		renderer = PIXI.autoDetectRenderer(320, 240);
+		renderer.view.style.position = "absolute";
+		renderer.view.style.top="0px";
+		renderer.view.style.left="0px";
+		renderer.view.style.border   = "1px solid";
+
+		document.body.appendChild(renderer.view);
 
 		// Set the initial position
 		var startX = 10,
 			startY = 10;
 
 		// Initialise the local player
-		localPlayer = new Player(startX, startY);
+		localPlayer = new Player(stage, startX, startY);
 	}();
 
 	var changePosition = function(x,y,width,height) {
-		canvas.width  = width;
-		canvas.height = height;
-		canvas.style.top=y+"px";
-		canvas.style.left=x+"px";
+		renderer.resize(width, height);
+		renderer.view.style.top=y+"px";
+		renderer.view.style.left=x+"px";
 	}
 
 	var update = function(keys) {
@@ -41,10 +36,12 @@ var Frame = function(id) {
 
 	var draw = function() {
 		// Wipe the canvas clean
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		//ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 		// Draw the local player
-		localPlayer.draw(ctx);
+		localPlayer.draw();
+		
+		renderer.render(stage);
 	};
 
 	return {
--- js/Player.js
+++ js/Player.js
@@ -1,10 +1,24 @@
 /**************************************************
 ** GAME PLAYER CLASS
 **************************************************/
-var Player = function(startX, startY) {
+var Player = function(stage,startX, startY) {
 	var x = startX,
 		y = startY,
-		moveAmount = 2;
+		moveAmount = 5,
+		texture,
+		bunny;
+
+	var init = function(stage) {
+		texture = PIXI.Texture.fromImage("assets/bunny.png");
+		// create a new Sprite using the texture
+		bunny = new PIXI.Sprite(texture);
+
+		// center the sprites anchor point
+		bunny.anchor.x = 0.5;
+		bunny.anchor.y = 0.5;
+
+		stage.addChild(bunny);
+	}(stage);
 
 	var update = function(keys) {
 		// Up key takes priority over down
@@ -22,8 +36,12 @@ var Player = function(startX, startY) {
 		};
 	};
 
-	var draw = function(ctx) {
-		ctx.fillRect(x-5, y-5, 10, 10);
+	var draw = function() {
+		bunny.position.x = x;
+		bunny.position.y = y;
+
+	    // just for fun, lets rotate mr rabbit a little
+	    bunny.rotation += 0.1;
 	};
 
 	return {
