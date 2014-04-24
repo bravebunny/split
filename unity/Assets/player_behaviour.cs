65b01bec7656b24ab46b0cd3fa0383c7a8bddc80
Makay11 <dpais11@gmail.com>
2014-04-24 14:14:20 +0100

    tweaked the jump

--- unity/Assets/player_behaviour.cs
+++ unity/Assets/player_behaviour.cs
@@ -6,7 +6,6 @@ public class player_behaviour : MonoBehaviour {
 	public float jumpForce = 1000f;
 
 	private float startingHeight;
-	//private float startingGravityScale;
 	
 	private bool jumping = false;
 	private bool sliding = false;
@@ -16,8 +15,7 @@ public class player_behaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		startingHeight = rigidbody.transform.position.y;
-		//startingGravityScale = llama.gravityScale;
-		Physics.gravity = new Vector3(0,-15,0);
+		Physics.gravity = new Vector3(0, -30, 0);
 
 		anim = GetComponent<Animator>();
 	}
@@ -26,7 +24,6 @@ public class player_behaviour : MonoBehaviour {
 	void Update () {
 		if (rigidbody.transform.position.y <= startingHeight) {
 			if (Mathf.Sign(rigidbody.velocity.y) < 0 && jumping){
-				//llama.gravityScale = 0f;
 				rigidbody.velocity = new Vector2(0, 0);
 				jumping = false;
 				anim.SetTrigger ("Grounded");
@@ -34,18 +31,15 @@ public class player_behaviour : MonoBehaviour {
 			rigidbody.transform.position = new Vector3(rigidbody.transform.position.x, startingHeight, rigidbody.transform.position.z);
 
 			if (Input.GetButtonDown("Jump")) {
-				//llama.gravityScale = startingGravityScale;
-				rigidbody.AddForce(new Vector3(0, jumpForce,0));
+				rigidbody.velocity = new Vector2(0, 20);
 				jumping = true;
 				anim.SetTrigger ("Jump");
 			}
 			else if (Input.GetButtonDown("Slide")) {
-				//llama.gravityScale = startingGravityScale;
 				sliding = true;
 				anim.SetTrigger ("Slide");
 			}
 			else if (Input.GetButtonDown("Spit")) {
-				//llama.gravityScale = startingGravityScale;
 				sliding = true;
 				anim.SetTrigger ("Spit");
 			}
