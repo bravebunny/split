cd52e3ed797ac1a86a88715e3cb28dbea856b80b
Makay11 <dpais11@gmail.com>
2014-04-24 04:29:48 +0100

    finally working properly

--- unity/Assets/player_behaviour.cs
+++ unity/Assets/player_behaviour.cs
@@ -5,18 +5,22 @@ public class player_behaviour : MonoBehaviour {
 
 	public Rigidbody2D llama;
 
+	public float startingHeight; 
+
 	// Use this for initialization
 	void Start () {
-	
+		startingHeight = llama.transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (llama.transform.position.y < 0) {
-			llama.transform.position.Set(llama.transform.position.x, llama.transform.position.y, 500);
-		}
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			llama.AddForce(new Vector2(0, 1000f));
+		if (llama.transform.position.y <= startingHeight) {
+			llama.transform.position = new Vector3(llama.transform.position.x, startingHeight, llama.transform.position.z);
+			llama.velocity = new Vector2(0, 0);
+
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				llama.AddForce(new Vector2(0, 1000f));
+			}
 		}
 	}
 }
