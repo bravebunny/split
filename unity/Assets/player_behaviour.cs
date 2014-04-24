a554804518a5f0afb741f00777238c0a489b1515
Makay11 <dpais11@gmail.com>
2014-04-24 05:18:54 +0100

    has collisions

--- unity/Assets/player_behaviour.cs
+++ unity/Assets/player_behaviour.cs
@@ -5,22 +5,36 @@ public class player_behaviour : MonoBehaviour {
 
 	public Rigidbody2D llama;
 
-	public float startingHeight; 
+	private float startingHeight;
+	private float startingGravityScale;
+
+	private bool jumping = false;
 
 	// Use this for initialization
 	void Start () {
 		startingHeight = llama.transform.position.y;
+		startingGravityScale = llama.gravityScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (llama.transform.position.y <= startingHeight) {
+			if (Mathf.Sign(llama.velocity.y) < 0){
+				llama.gravityScale = 0f;
+				llama.velocity = new Vector2(0, 0);
+				jumping = false;
+			}
 			llama.transform.position = new Vector3(llama.transform.position.x, startingHeight, llama.transform.position.z);
-			llama.velocity = new Vector2(0, 0);
 
 			if (Input.GetKeyDown(KeyCode.Space)) {
+				llama.gravityScale = startingGravityScale;
 				llama.AddForce(new Vector2(0, 1000f));
+				jumping = true;
 			}
+		}
+
+		if (jumping) {
+			//animate
 		}
 	}
 }
---
+++ unity/Assets/stone_behaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stone_behaviour : MonoBehaviour {
+
+	public Rigidbody2D rigidbody;
+
+	public float speed = -10;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = new Vector3 (speed, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
